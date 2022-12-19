9e464b9c263ae86675c95803816670ce2b96d7b3
Vitaly Sorokin <vityq.dev@gmail.com>
2022-12-19 16:52:11 +0300

    Initial commit

---
+++ app/src/main/java/com/vityq/testwork/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.vityq.testwork
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vityq.testwork.ui.theme.TestWorkTheme
+import com.vityq.testwork.ui.view.BackgroundView
+import java.util.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestWorkTheme {
+
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    BackgroundView()
+                    Greeting("Android")
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestWorkTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/vityq/testwork/ui/view/BackgroundView.kt
@@ -0,0 +1,65 @@
+package com.vityq.testwork.ui.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.vityq.testwork.ui.theme.TestWorkTheme
+import com.vityq.testwork.ui.theme.mainBackground
+
+@Composable
+fun BackgroundView() {
+    ConstraintLayout(
+        Modifier
+            .fillMaxSize()
+            .background(mainBackground)
+    ) {
+        val (char1, char2, char3, char4, char5, char6, char7, char8, char9, char10, char11)
+                = createRefs()
+
+        Text(
+            text = "!", Modifier.constrainAs(char1) {
+                top.linkTo(parent.top, 24.dp)
+                end.linkTo(parent.end, 24.dp)
+            },
+            style = MaterialTheme.typography.h6
+        )
+        Text(
+            text = "?", Modifier.constrainAs(char2) {
+                top.linkTo(char1.bottom, (-24).dp)
+                start.linkTo(parent.start, 24.dp)
+            },
+            style = MaterialTheme.typography.h6
+        )
+        Text(
+            text = "!", Modifier.constrainAs(char3) {
+                top.linkTo(char2.bottom, (-16).dp)
+                start.linkTo(char2.end, 32.dp)
+            },
+            style = MaterialTheme.typography.h6
+        )
+        Text(
+            text = "?", Modifier.constrainAs(char4) {
+                end.linkTo(char1.start)
+                top.linkTo(char3.bottom, (-32).dp)
+            },
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@Preview(widthDp = 320, heightDp = 640)
+@Composable
+fun BackgroundPreview() {
+    TestWorkTheme {
+        BackgroundView()
+    }
+}
