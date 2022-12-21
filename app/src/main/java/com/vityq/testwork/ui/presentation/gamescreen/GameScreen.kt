30f09f8e41bde1343b40a1f2cd88df19e354d063
Vitaly Sorokin <vityq.dev@gmail.com>
2022-12-21 17:45:43 +0300

    2nd lesson

--- app/src/main/java/com/vityq/testwork/ui/presentation/gamescreen/GameScreen.kt
+++ app/src/main/java/com/vityq/testwork/ui/presentation/gamescreen/GameScreen.kt
@@ -4,15 +4,15 @@ import android.widget.Space
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vityq.testwork.ui.theme.TestWorkTheme
 import com.vityq.testwork.ui.theme.headerColor
 import com.vityq.testwork.ui.theme.scoreColor
@@ -30,7 +30,9 @@ fun GameScreen() {
             shape = RoundedCornerShape(16.dp)
         ) {
             Column(
-                modifier = Modifier.wrapContentSize().padding(all = 8.dp),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(all = 8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
@@ -49,15 +51,60 @@ fun GameScreen() {
             contentColor = Color.White,
             shape = CutCornerShape(20),
             elevation = 8.dp,
-
         ) {
             Column(
-                modifier = Modifier.wrapContentSize().padding(all = 8.dp),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(all = 8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = "34657", style = MaterialTheme.typography.h4)
             }
         }
+        Spacer(modifier = Modifier.height(64.dp))
+        Card(
+            Modifier
+                .padding(horizontal = 32.dp)
+        )
+        {
+            Box(modifier = Modifier.padding(horizontal = 32.dp, vertical = 8.dp)) {
+                Text(
+                    text = "8 + 6",
+                    color = headerColor,
+                    fontSize = 48.sp,
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    ),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.width(64.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.White
+                )
+            ) {
+                Text(text = "32", color = headerColor)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.White
+                )
+            ) {
+                Text(text = "16", color = headerColor)
+            }
+            Spacer(modifier = Modifier.width(64.dp))
+        }
+
+        Spacer(modifier = Modifier.height(128.dp))
     }
 }
 
